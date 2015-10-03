df835ebb5b8d20575936a6ad62e44a58dccb9f96
yinguohang <jinglingshen@126.com>
2015-10-03 20:43:34 +0800

    login finished

---
+++ zuoyehezi/zuoyehezi/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for zuoyehezi project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'zuoyehezi'
+
+SPIDER_MODULES = ['zuoyehezi.spiders']
+NEWSPIDER_MODULE = 'zuoyehezi.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'zuoyehezi (+http://www.yourdomain.com)'
